bccf724cda84a30ea584713451f535d1599afc7e
RafaelViolas <rafaelmachadoviola@enta.pt>
2024-06-03 10:19:33 +0000

    rewrote the HTTPHandler so it works with the html file being located in webpage

--- server.py
+++ server.py
@@ -1,10 +1,14 @@
 import http.server
 import socketserver
+import os
 
-PORT = 6000
+PORT = 7000
+DIRECTORY = "webpage"
 
-Handler = http.server.SimpleHTTPRequestHandler
+class CustomHandler(http.server.SimpleHTTPRequestHandler):
+     def __init__(self, *args, **kwargs):
+         super().__init__(*args, directory=DIRECTORY, **kwargs)
 
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
+with socketserver.TCPServer(("", PORT), CustomHandler) as httpd:
     print(f"Serving at port {PORT}")
     httpd.serve_forever()
